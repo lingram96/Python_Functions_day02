20e7699ce424983504261af9434ce35a891779d2
Lewis Ingram <lewisingram@Lewiss-MacBook-Pro-2.local>
2021-08-31 19:27:39 +0100

    first commit

---
+++ my_functions.py
@@ -0,0 +1,31 @@
+# def greet(name, time_of_day):
+#     return "Good " + time_of_day + ", " + name
+
+# name_1 = "Lewis"
+# time_of_day_1 = "morning"
+# greeting = greet(name_1, time_of_day_1)
+# print(greeting)
+
+# name_2 = "Dave"
+# time_of_day_2 = "evening"
+# greeting = greet(name_2, time_of_day_2)
+# print(greeting)
+
+chickens = [
+    { "name": "Margaret", "age": 2, "eggs": 0 },
+    { "name": "Hetty", "age": 1, "eggs": 2 },
+    { "name": "Henrietta", "age": 3, "eggs": 1 },
+    { "name": "Audrey", "age": 2, "eggs": 0 },
+    { "name": "Mabel", "age": 5, "eggs": 1 },
+]
+
+def count_eggs(list):
+    total_eggs = 0 
+
+    for bird in list:
+        total_eggs += bird["eggs"]
+        bird["eggs"] = 0
+
+    return f"{total_eggs} eggs collected"
+
+print(count_eggs(chickens))
